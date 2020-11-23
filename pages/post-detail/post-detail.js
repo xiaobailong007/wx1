c27e635997ab971ea803e9cfd410726ea57bf2ac
DESKTOP-JIUDF9M\Administrator <15820197555@163.com>
2020-11-23 23:57:36 +0800

    1

--- pages/post-detail/post-detail.js
+++ pages/post-detail/post-detail.js
@@ -1,4 +1,6 @@
-import {postList} from '../../data/data'
+import {
+  postList
+} from '../../data/data'
 Component({
   /**
    * 组件的属性列表
@@ -11,35 +13,38 @@ Component({
    * 组件的初始数据
    */
   data: {
-    postData:{},
-    collected:false,
-    _pid:null,
+    postData: {},
+    collected: false,
+    _pid: null,
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    onLoad:function(options){
+    onLoad: function (options) {
       const postData = postList[options.pid]
       this.data._pid = options.pid
       const postsCollected = wx.getStorageSync('postsCollected')
-      const collected = postsCollected[this.data._pid]
-   
+      let collected = postsCollected[this.data._pid]
+      if (collected === undefined) {
+        //如果undefined,说明文章从没有被收藏过
+        collected = false
+      }
       this.setData({
         postData,
         collected
       })
     },
-    onCollect(event){
-//未收藏
-const postsCollected ={};
-postsCollected[this.data._pid]=true
-this.data.collected = true
-this.setData({
-  collected:this.data.collected
-})
-wx.setStorageSync('posts_collected',postsCollected)
+    onCollect(event) {
+      //未收藏
+      const postsCollected = {};
+      postsCollected[this.data._pid] = true
+      this.data.collected = true
+      this.setData({
+        collected: this.data.collected
+      })
+      wx.setStorageSync('posts_collected', postsCollected)
 
     }
   }
